5169df8f42eaf536a9a67f26a05410c59b3f6771
ritik <ritikbhatt020@gmail.com>
2023-10-22 01:41:52 +0530

    Final changes of Diabetes model API

--- app.py
+++ app.py
@@ -13,16 +13,22 @@ def home():
     return "Hello World here"
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predictD', methods=['POST'])
 def predict():
     Pregnancies = request.form.get('Pregnancies')
-    cgpa = request.form.get('cgpa')
-    cgpa = request.form.get('cgpa')
-    cgpa = request.form.get('cgpa')
-    cgpa = request.form.get('cgpa')
-    cgpa = request.form.get('cgpa')
-    cgpa = request.form.get('cgpa')
-    cgpa = request.form.get('cgpa')
+    Glucose = request.form.get('Glucose')
+    BloodPressure = request.form.get('BloodPressure')
+    SkinThickness = request.form.get('SkinThickness')
+    Insulin = request.form.get('Insulin')
+    BMI = request.form.get('BMI')
+    DiabetesPedigreeFunction = request.form.get('DiabetesPedigreeFunction')
+    Age = request.form.get('Age')
+
+    input_query = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]], dtype=np.float64)
+
+    result = model.predict(input_query)[0]
+
+    return jsonify({'Outcome of Diabetes': str(result)})
 
 
 if __name__ == '__main__':
